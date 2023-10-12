b7c3a0867b4629e127ce0e9e49328e3ebe5410ea
Omkar <omkarkamble1235@gmail.com>
2023-10-13 02:17:04 +0530

    added frontend dir

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+	<div className='flex justify-center h-screen items-center'>
+		<h1 className="text-5xl font-bold">Attendance Dashboard</h1>
+	</div>
+  );
+}
+
+export default App
